c20b1dd296e4f994049e3952afec0a5f6f5f6416
Fredy Mora Santillan <fredy.morasantillan@gmail.com>
2022-01-22 11:06:51 -0600

    Actividad 2

---
+++ Actividades/Actividad 2/src/main/java/uia/com/api/mvc/apimvc/IndexController.java
@@ -0,0 +1,19 @@
+package uia.com.api.mvc.apimvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+class IndexController {
+
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    @ResponseBody
+    public String showIndex(){
+        return "Actividad 2 " +
+                "de la materia " +
+                "Arquitectura de Software";
+    }
+
+}
