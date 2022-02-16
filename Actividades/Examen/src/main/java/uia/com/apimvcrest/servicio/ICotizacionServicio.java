49093b0b678e0d81fe541884e0de033251d63188
Fredy Mora Santillan <fredy.morasantillan@gmail.com>
2022-02-15 22:24:33 -0600

    examen

---
+++ Actividades/Examen/src/main/java/uia/com/apimvcrest/servicio/ICotizacionServicio.java
@@ -0,0 +1,22 @@
+package uia.com.apimvcrest.servicio;
+
+import uia.com.apimvcrest.compras.InfoComprasUIA;
+import uia.com.apimvcrest.modelo.CotizacionModelo;
+import uia.com.apimvcrest.modelo.ItemComprasUIAModelo;
+
+import java.util.ArrayList;
+
+public interface ICotizacionServicio {
+    ArrayList<CotizacionModelo> getCotizaciones();
+    Object getCotizacion(int id);
+    void print();
+
+    CotizacionModelo deleteCotizacion(int id);
+    CotizacionModelo updateCotizacionById(int id, CotizacionModelo cotizacionById);
+    /*public CotizacionModelo updateCotizacionById(int id, CotizacionModelo cotizacionById)
+        {
+            //return miGestorCompras.updateCotizacionById(id, cotizacionById);
+            return miGestorCompras.updateCotizacionById(id, cotizacionById);
+        }*/
+
+}
