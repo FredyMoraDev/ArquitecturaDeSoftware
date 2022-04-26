76c55e583eb9cb73349f8317e1bffe5ab827a0c5
Fredy Mora Santillan <fredy.morasantillan@gmail.com>
2022-04-26 00:43:56 -0500

    probando otros patrones

---
+++ api-rest-comprasUIA-5/src/main/java/uia/com/apirest/patronesDisenno/DecoradorSolicitudOC.java
@@ -0,0 +1,49 @@
+package uia.com.apirest.patronesDisenno;
+
+import uia.com.apirest.compras.InfoComprasUIA;
+import uia.com.apirest.compras.PeticionOrdenCompra;
+
+import java.util.List;
+
+public class DecoradorSolicitudOC extends PeticionOrdenCompra implements IListaInfoComprasUIA {
+    protected  ReporteNivelStockConcreto reporte;
+
+    public DecoradorSolicitudOC(ReporteNivelStockConcreto SolicitudOrdenCompra)
+    {
+        System.out.println("DecoradorSolicitudOC");
+        this.reporte=SolicitudOrdenCompra;
+        this.setItems(this.reporte.agregaItems());
+        System.out.println("DecoradorSolicitudOC");
+    }
+
+    @Override
+    public List<InfoComprasUIA> getItems(){
+        return super.getItems();
+    }
+
+    @Override
+    public void setItems(List<InfoComprasUIA> l)
+    {
+        super.setItems(l);
+    }
+
+    @Override
+    public int getId() {
+        return 0;
+    }
+
+    @Override
+    public void setId(int id) {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public void setName(String name) {
+
+    }
+}
